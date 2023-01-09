be4a74ed4f932d023dbda2912bfcd9cdbc7c68e8
Ronan Martin <ronanmartin86@outlook.com>
2023-01-09 18:40:20 -0300

    Criando um Programa em Go para a Conversao de Escalas Termometricas

---
+++ Convertion.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+const ebulicaoK = 373.15
+
+func main() {
+
+	tempK := ebulicaoK
+	tempC := tempK - 273.15 // Conversão de Kelvin para Celsius
+
+	fmt.Printf("A temperatura de ebulição da água em °K = %g e a temperatura de ebulição da água em °C = %g.", tempK, tempC)
+}
