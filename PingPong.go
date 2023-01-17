7e1c5a30a6855906f8d72a63b57c2de47645f296
Ronan Martin <ronanmartin86@outlook.com>
2023-01-17 18:55:04 -0300

    Ping Pong com GO

---
+++ PingPong.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+
+	var c chan string = make(chan string)
+
+	wg.Add(2)
+
+	go Ping(c)
+	go imprimir(c)
+
+	wg.Wait()
+	//var entrada string
+	//fmt.Scanln(&entrada)  - Desta forma não seria necessário o WaitGroups
+
+}
+
+var wg sync.WaitGroup
+
+func Ping(c chan string) {
+	for i := 0; i < 10; i++ { // Também poderia ser infinito, bastaria utilizar "ctrl + c" para sair
+		c <- "Ping"
+		c <- "Pong"
+	}
+	wg.Done()
+}
+
+func imprimir(c chan string) {
+	for i := 0; i < 20; i++ {
+		//msg := <-c
+		//fmt.Println (msg) - Seria outra forma de printar, utilizando o Scanln, conforme comentário sobre WaitGroups.
+		fmt.Println(<-c)
+		time.Sleep(time.Second * 1)
+	}
+	wg.Done()
+}
